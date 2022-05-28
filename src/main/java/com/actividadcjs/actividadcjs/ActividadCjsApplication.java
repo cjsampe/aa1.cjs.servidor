7673edcf98b017eac99f1b095d023c962688a806
cjsampe <samperiz@hotmail.com>
2022-05-28 17:54:57 +0200

    proyecto inicial

---
+++ src/main/java/com/actividadcjs/actividadcjs/ActividadCjsApplication.java
@@ -0,0 +1,13 @@
+package com.actividadcjs.actividadcjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadCjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadCjsApplication.class, args);
+	}
+
+}
